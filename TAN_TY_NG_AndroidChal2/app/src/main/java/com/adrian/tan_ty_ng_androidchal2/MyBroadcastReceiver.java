6d60961cafe0199faeea9a43f2e37d69e9b7969e
ATy117 <41797980+ATy117@users.noreply.github.com>
2019-11-19 12:42:14 +0800

    created broadcast receiver

---
+++ TAN_TY_NG_AndroidChal2/app/src/main/java/com/adrian/tan_ty_ng_androidchal2/MyBroadcastReceiver.java
@@ -0,0 +1,21 @@
+package com.adrian.tan_ty_ng_androidchal2;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Vibrator;
+import android.widget.Toast;
+
+public class MyBroadcastReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Toast.makeText(context, "Alarm is alarming", Toast.LENGTH_LONG).show();
+
+        Vibrator vibrator = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
+        vibrator.vibrate(50000);
+
+        //TODO Play Sound
+
+        //TODO Create Notification
+    }
+}
