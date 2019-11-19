25de558ae1fe7fda5e9378e7db7505d2ae9e6fcc
schuyl3r <schuyler_ng@dlsu.edu.ph>
2019-11-19 16:18:59 +0800

    VIBRATE FUCKING WORKS

---
+++ TAN_TY_NG_AndroidChal2/app/src/main/java/com/adrian/tan_ty_ng_androidchal2/YourService.java
@@ -0,0 +1,24 @@
+package com.adrian.tan_ty_ng_androidchal2;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.IBinder;
+
+public class YourService extends Service {
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        // do your jobs here
+        return super.onStartCommand(intent, flags, startId);
+    }
+}
