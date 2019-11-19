c90fa33826f7b086e99a03a9479bad91c726c3db
ATy117 <41797980+ATy117@users.noreply.github.com>
2019-11-19 13:05:44 +0800

    check if pet exists is goods

--- TAN_TY_NG_AndroidChal2/app/src/main/java/com/adrian/tan_ty_ng_androidchal2/PetActivity.java
+++ TAN_TY_NG_AndroidChal2/app/src/main/java/com/adrian/tan_ty_ng_androidchal2/PetActivity.java
@@ -2,20 +2,30 @@ package com.adrian.tan_ty_ng_androidchal2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class PetActivity extends AppCompatActivity {
 
     TextView statusTextView, messageTextView;
     Button snackButton, mealButton, kingButton, releaseButton;
 
+    SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pet);
+        sharedPreferences = getSharedPreferences("android_chal_2", Context.MODE_PRIVATE);
 
         statusTextView = findViewById(R.id.statusTextView);
         messageTextView = findViewById(R.id.messageTextView);
@@ -23,5 +33,18 @@ public class PetActivity extends AppCompatActivity {
         mealButton = findViewById(R.id.mealButton);
         kingButton = findViewById(R.id.kingButton);
         releaseButton = findViewById(R.id.releaseButton);
+    }
+
+    public void startAlert(int seconds) {
+        EditText text = (EditText) findViewById(R.id.time);
+        int i = Integer.parseInt(text.getText().toString());
+        Intent intent = new Intent(this, MyBroadcastReceiver.class);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(
+                this.getApplicationContext(), 234324243, intent, 0);
+        AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+        alarmManager.set(AlarmManager.RTC_WAKEUP, System.currentTimeMillis()
+                + (seconds * 1000), pendingIntent);
+        Toast.makeText(this, "Alarm set in " + i + " seconds",
+                Toast.LENGTH_LONG).show();
     }
 }
